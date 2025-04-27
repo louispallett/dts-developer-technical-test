9b8d509c2c062f33fdab427f4473140f467c62cb
louispallett <louis.pallett@outlook.com>
2025-04-28 00:11:17 +0100

    Update various variables to connect to proxy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,15 +5,15 @@ import macrosPlugin from 'vite-plugin-babel-macros';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [macrosPlugin(), react()],
-  server: {
-    port: 3001,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    }    
-  },
+  // server: {
+  //   port: 3001,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000/',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       ws: true,
+  //     },
+  //   }    
+  // },
 });
--- server/app.js
+++ server/app.js
@@ -55,5 +55,9 @@ app.use(session({
 
 app.use("/api/task", taskRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
+});
